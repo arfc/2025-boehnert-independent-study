1111add5cc633ee89a5ac68689c1f15a3070a9f4
Thomas Bohnert <tbohnert.2018@gmail.com>
2025-04-30 17:10:32 -0500

    Added Random Ray script, created analysis notebook for comparing results

---
+++ lab_notebook/generate_rr_model.py
@@ -0,0 +1,15 @@
+import openmc
+
+model = openmc.Model.from_xml()
+model.convert_to_multigroup()
+model.convert_to_random_ray()
+
+n=100
+mesh=openmc.RegularMesh()
+mesh.dimension = (n,n)
+mesh.lower_left = model.geometry.bounding_box.lower_left
+mesh.upper_right = model.geometry.bounding_box.upper_right
+model.settings.random_ray['source_region_meshes'] = [(mesh, [model.geometry.root_universe])]
+model.settings.random_ray['volume_normalized_flux_tallies'] = True
+model.export_to_xml()
+
