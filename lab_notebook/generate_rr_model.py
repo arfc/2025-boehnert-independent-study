924e3a31a799d5c6567e72c2ec449bf24ef5816d
Thomas Bohnert <tbohnert.2018@gmail.com>
2025-05-02 13:14:23 -0500

    Changes to methods and results, added Analysis notebook and generate_rr_model

--- lab_notebook/generate_rr_model.py
+++ lab_notebook/generate_rr_model.py
@@ -10,6 +10,7 @@ mesh.dimension = (n,n)
 mesh.lower_left = model.geometry.bounding_box.lower_left
 mesh.upper_right = model.geometry.bounding_box.upper_right
 model.settings.random_ray['source_region_meshes'] = [(mesh, [model.geometry.root_universe])]
-model.settings.random_ray['volume_normalized_flux_tallies'] = True
+model.settings.random_ray['volume_normalized_flux_tallies'] = False
+model.settings.entropy_mesh = mesh
 model.export_to_xml()
 
